3b553888c2f45522600603d76e0f08866a7d9ba4
zhuchentong <2037630@163.com>
2021-12-03 10:22:35 +0800

    添加更新检测支持

--- src/bootstrap/launch/app.launch.ts
+++ src/bootstrap/launch/app.launch.ts
@@ -1,7 +1,13 @@
+import { UpdateService } from '@/shared/utils/update.service'
+
+async function appUpdateCheck() {
+    UpdateService.checkUpdate()
+}
+
 /**
  * 系统启动列表
  * @returns
  */
 export default function appLaunch() {
-    return []
+    return [appUpdateCheck()]
 }
